ff0a13c86c2eef47080c3e19ba12139069408637
JoysonAJ <joysondsouzaaj@gmail.com>
2025-10-27 17:04:31 +0530

    context provider added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import RoutePage from "@/routes/Route.page"
+import AuthProvider from "@/context/AuthContext"
 
 function App() {
   
   return (
-    <>
+    <AuthProvider>
       <RoutePage />
-    </>
+    </AuthProvider>
   )
 }
 
