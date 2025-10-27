bd9a6821686c059158c0e7b571ec4f89c4fa037b
JoysonAJ <joysondsouzaaj@gmail.com>
2025-10-27 17:03:41 +0530

    config of protected route

--- src/routes/Route.page.tsx
+++ src/routes/Route.page.tsx
@@ -1,18 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "@/layout/Layout";
-import { loginRoute,registerRoute } from "@/routes/route";
+import { loginRoute, registerRoute } from "@/routes/route";
 import Register from "@/pages/auth/Register";
 import Login from "@/pages/auth/Login";
+import HomePage from "@/pages/home/HomePage";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function RoutePage() {
   return (
     <BrowserRouter>
-        <Routes>
+      <Routes>
         <Route path="" element={<Layout />}>
-        <Route path={registerRoute.path} element={<Register />} />
-        <Route path={loginRoute.path} element={<Login/>} />
+          <Route path={loginRoute.path} element={<Login />} />
+          <Route path={registerRoute.path} element={<Register />} />
+          <Route
+            path={"/home"}
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
         </Route>
-        </Routes>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
